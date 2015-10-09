734e03a061745a7ab4e8e54f02a1b10e62194499
Michaela Voorhees <micha.voorhees@gmail.com>
2015-10-08 20:27:18 -0700

    ing_matrix makes prediction on test data using cuisine-ingredients matrix from training data. Prints first 10 predictions.

--- ing_matrix.py
+++ ing_matrix.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import json
-import pandas as pd
+import numpy as np
+from sklearn.feature_extraction import DictVectorizer
 
 trainfile = open('train1.json')
 train = json.loads(trainfile.read())
@@ -16,7 +17,31 @@ for cuisine, ingredients in inglist_by_cui.iteritems():
     for ingredient in ingredients:
         ingfreq_by_cui[cuisine][ingredient] += 1 
 
-for ingredients in ingfreq_by_cui.values():
-    for ingredient in ingredients.keys():
-        if ',' in ingredient:
-            print ingredient
+cuisine_axis = []
+ingfreq_list = []
+for cuisine, ing_hist in ingfreq_by_cui.iteritems():
+     cuisine_axis.append(cuisine)
+     ingfreq_list.append(ing_hist)
+
+vec = DictVectorizer()
+ing_array = vec.fit_transform(ingfreq_list)
+
+# open test file
+testfile = open('train2.json')
+test = json.loads(testfile.read())
+testfile.close()
+
+#make predictions with test data
+pred_ids = []
+predictions = []
+for id, ing_list in test['ingredients'].iteritems():
+    pred_ids.append(id)
+    ingfreq = defaultdict(int)
+    for ingredient in ing_list:
+        ingfreq[ingredient] += 1
+    pred_vec = vec.transform(ingfreq).transpose()
+    cui_sim = ing_array.dot(pred_vec).todense().tolist()
+    prediction = cuisine_axis[cui_sim.index(max(cui_sim))]
+    predictions.append(prediction)
+
+print predictions[:10]
