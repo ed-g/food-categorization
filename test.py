320edc0f7b6de266d425407c1838b169866dc79d
Michaela Voorhees <micha.voorhees@gmail.com>
2015-11-03 14:37:36 -0800

    Added some tests to tests.py for first 3 functions of ing_matrix.

--- test.py
+++ test.py
@@ -8,6 +8,26 @@ from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import normalize
 import toolz
 
+from ing_matrix import *
+
+
+def test_get_data():
+    assert type(get_data("data/train1.json")) == dict
+
+PRACTICE_DATA = {"cuisine":{1:'a', 2:'c', 3:'a', 4:'a'},
+                 "ingredients":{1:['as', 'bs', 'cs'], 2:['ds', 'es'], 3:['bs', 'es'],
+                                4:['bs']}}
+
+def test_group_ing_by_cui():
+    assert len(group_ingredients_by_cuisine(PRACTICE_DATA)['a']) == 6
+    assert len(group_ingredients_by_cuisine(PRACTICE_DATA)['c']) == 2
+
+PRACTICE_INGLISTS = {'a': ['as', 'bs', 'cs', 'bs', 'es', 'bs'], 'c': ['ds', 'es']}
+
+def test_count_ingredients():
+    assert len(count_ingredients_in_cuisine(PRACTICE_INGLIST)['a']['bs']) == 3
+    assert len(count_ingredients_in_cuisine(PRACTICE_INGLIST)['c']['es']) == 1
+
 
 # Test how maps work.
 def test_map():
@@ -54,4 +74,4 @@ def test_normalize():
     print ("ary = normalize(ary, axis=0)\n", ary.toarray())
 if __name__ == '__main__':
     test_normalize()
-
+    test_group_ing_by_cui()
